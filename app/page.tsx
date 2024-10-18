83de6e120ce94403e718a431eb8a17dbed41cf31
bhavana1111 <bhavanakondeti2000@gmail.com>
2024-10-18 14:25:59 -0500

    Home Page edited

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,21 @@
-import { BlogPosts } from 'app/components/posts'
+import { BlogPosts } from "app/components/posts";
 
 export default function Page() {
   return (
     <section>
       <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
-        My Portfolio
+        Bhavana Kondeti
       </h1>
       <p className="mb-4">
-        {`I'm a Vim enthusiast and tab advocate, finding unmatched efficiency in
-        Vim's keystroke commands and tabs' flexibility for personal viewing
-        preferences. This extends to my support for static typing, where its
-        early error detection ensures cleaner code, and my preference for dark
-        mode, which eases long coding sessions by reducing eye strain.`}
+        {`I'm Bhavana Kondeti currently working as a `}
+        <span className="font-bold text-indigo-600">
+          Research Assistant at UTSA
+        </span>
+        {`, with prior experience as a developer. My expertise spans frontend development, particularly as a JavaScript and React developer, as well as backend development with C# and .NET. I am also involved in cutting-edge projects utilizing Large Language Models (LLMs). Passionate about technology and solving complex problems, I am always eager to learn and contribute to innovative solutions in the ever-evolving tech world.`}
       </p>
       <div className="my-8">
         <BlogPosts />
       </div>
     </section>
-  )
+  );
 }
